2eb7656fe373e7d06faf0a98e96291eb768a1667
SUJIANPING-Dia <da730101@gmail.com>
2026-04-01 14:35:30 +0800

    test api

--- src/pages/api/cloudinary.js
+++ src/pages/api/cloudinary.js
@@ -1,15 +1,16 @@
 import { v2 as cloudinary } from 'cloudinary';
 
-cloudinary.config({
-  cloud_name: 'dahniduay',
-  api_key: '371257267424889',
-  api_secret: 'lXOLsgktpVS2EgiBHwxp5p-5L_A',
-});
-
 export async function GET() {
+
+  cloudinary.config({
+    cloud_name: 'dahniduay',
+    api_key: '你的API_KEY',
+    api_secret: '你的API_SECRET',
+  });
+
   try {
     const result = await cloudinary.search
-      .expression('folder="events/ICCT-Pacific 2026"')
+      .expression('folder:"events/ICCT-Pacific 2026"')
       .sort_by('created_at', 'desc')
       .max_results(200)
       .execute();
@@ -17,15 +18,16 @@ export async function GET() {
     return new Response(JSON.stringify(result.resources), {
       status: 200,
       headers: {
-        "Content-Type": "application/json",
-      },
+        'Content-Type': 'application/json'
+      }
     });
+
   } catch (error) {
     return new Response(JSON.stringify({ error: error.message }), {
       status: 500,
       headers: {
-        "Content-Type": "application/json",
-      },
+        'Content-Type': 'application/json'
+      }
     });
   }
 }
