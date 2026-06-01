6a181555bdb2bd36783a684c17e57c7af757d082
SUJIANPING-Dia <da730101@gmail.com>
2026-06-01 16:34:03 +0800

    Add videos page

---
+++ src/data/videos.ts
@@ -0,0 +1,19 @@
+export type VideoItem = {
+  title: string;
+  youtubeId: string;
+};
+
+export const videos: VideoItem[] = [
+  {
+    title: "實驗室影片 1",
+    youtubeId: "FYVEmAv1t38",
+  },
+  {
+    title: "實驗室影片 2",
+    youtubeId: "x6QEfvkknOM",
+  },
+  {
+    title: "實驗室影片 3",
+    youtubeId: "1LYw3fPlm5s",
+  },
+];
