a02ad9fed4231abf9622042679f4fd8fa2fe17d3
SUJIANPING-Dia <da730101@gmail.com>
2026-06-26 10:19:10 +0800

    Add video item: Medical Taiwan 2026

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -5,6 +5,10 @@ export type VideoItem = {
 
 export const videos: VideoItem[] = [
   {
+    title: "三立新聞報導｜2026 台灣國際醫療暨健康照護展 iCPS Lab AI 外科手術器械識別技術",
+    youtubeId: "ou1L5Okv2eA",
+  },
+  {
     title: "2026醫學教育國際研討會 | 高雄醫學大學 | iCps Lab",
     youtubeId: "CWx9cUO4ym0",
   },
